7e6793aae342ffc7816c75fd7d64702c69125c16
Kelell Davison-Thomas [ll16kdt] <ll16kdt@comp-pc6168.leeds.ac.uk>
2018-11-13 15:56:55 +0000

    area for the coursework

---
+++ cwk2/main.cc
@@ -0,0 +1,11 @@
+// c++ hello world
+// Author: Felix (scsfsa)
+// Date: Oct 2018
+
+#include <iostream>
+
+int main(){
+	std::cout << "No Git repository found.\n\nCreate a new Git repository?\n\n";
+	std::cout << "y yes\nn no, quit\n";
+
+}
